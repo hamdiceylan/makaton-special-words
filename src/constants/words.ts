2f7bc6371bca81de13113210d5952c88514a7f36
Burak Köse <iosdeveloper19@hotmail.com>
2025-09-15 23:20:40 +0300

    Add words data

---
+++ src/constants/words.ts
@@ -0,0 +1,779 @@
+export interface Word {
+  image: string;
+  imageUpdated: boolean;
+  sound: string;
+  soundUpdated: boolean;
+  text: string;
+  textUpdated: boolean;
+}
+
+export const words: Word[] = [
+  {
+    "image": "ball",
+    "imageUpdated": false,
+    "sound": "ball",
+    "soundUpdated": false,
+    "text": "ball",
+    "textUpdated": false
+  },
+  {
+    "image": "book",
+    "imageUpdated": false,
+    "sound": "book",
+    "soundUpdated": false,
+    "text": "book",
+    "textUpdated": false
+  },
+  {
+    "image": "dolly",
+    "imageUpdated": false,
+    "sound": "dolly",
+    "soundUpdated": false,
+    "text": "dolly",
+    "textUpdated": false
+  },
+  {
+    "image": "car",
+    "imageUpdated": false,
+    "sound": "car",
+    "soundUpdated": false,
+    "text": "car",
+    "textUpdated": false
+  },
+  {
+    "image": "bird",
+    "imageUpdated": false,
+    "sound": "bird",
+    "soundUpdated": false,
+    "text": "bird",
+    "textUpdated": false
+  },
+  {
+    "image": "dog",
+    "imageUpdated": false,
+    "sound": "dog",
+    "soundUpdated": false,
+    "text": "dog",
+    "textUpdated": false
+  },
+  {
+    "image": "cat",
+    "imageUpdated": false,
+    "sound": "cat",
+    "soundUpdated": false,
+    "text": "cat",
+    "textUpdated": false
+  },
+  {
+    "image": "fish",
+    "imageUpdated": false,
+    "sound": "fish",
+    "soundUpdated": false,
+    "text": "fish",
+    "textUpdated": false
+  },
+  {
+    "image": "flower",
+    "imageUpdated": false,
+    "sound": "flower",
+    "soundUpdated": false,
+    "text": "flower",
+    "textUpdated": false
+  },
+  {
+    "image": "keys",
+    "imageUpdated": false,
+    "sound": "keys",
+    "soundUpdated": false,
+    "text": "keys",
+    "textUpdated": false
+  },
+  {
+    "image": "cup",
+    "imageUpdated": false,
+    "sound": "cup",
+    "soundUpdated": false,
+    "text": "cup",
+    "textUpdated": false
+  },
+  {
+    "image": "brush",
+    "imageUpdated": false,
+    "sound": "brush",
+    "soundUpdated": false,
+    "text": "brush",
+    "textUpdated": false
+  },
+  {
+    "image": "coat",
+    "imageUpdated": false,
+    "sound": "coat",
+    "soundUpdated": false,
+    "text": "coat",
+    "textUpdated": false
+  },
+  {
+    "image": "shoes",
+    "imageUpdated": false,
+    "sound": "shoes",
+    "soundUpdated": false,
+    "text": "shoes",
+    "textUpdated": false
+  },
+  {
+    "image": "hat",
+    "imageUpdated": false,
+    "sound": "hat",
+    "soundUpdated": false,
+    "text": "hat",
+    "textUpdated": false
+  },
+  {
+    "image": "socks",
+    "imageUpdated": false,
+    "sound": "socks",
+    "soundUpdated": false,
+    "text": "socks",
+    "textUpdated": false
+  },
+  {
+    "image": "duck",
+    "imageUpdated": false,
+    "sound": "duck",
+    "soundUpdated": false,
+    "text": "duck",
+    "textUpdated": false
+  },
+  {
+    "image": "pig",
+    "imageUpdated": false,
+    "sound": "pig",
+    "soundUpdated": false,
+    "text": "pig",
+    "textUpdated": false
+  },
+  {
+    "image": "sheep",
+    "imageUpdated": false,
+    "sound": "sheep",
+    "soundUpdated": false,
+    "text": "sheep",
+    "textUpdated": false
+  },
+  {
+    "image": "cow",
+    "imageUpdated": false,
+    "sound": "cow",
+    "soundUpdated": false,
+    "text": "cow",
+    "textUpdated": false
+  },
+  {
+    "image": "apple",
+    "imageUpdated": false,
+    "sound": "apple",
+    "soundUpdated": false,
+    "text": "apple",
+    "textUpdated": false
+  },
+  {
+    "image": "banana",
+    "imageUpdated": false,
+    "sound": "banana",
+    "soundUpdated": false,
+    "text": "banana",
+    "textUpdated": false
+  },
+  {
+    "image": "biscuit",
+    "imageUpdated": false,
+    "sound": "biscuit",
+    "soundUpdated": false,
+    "text": "biscuit",
+    "textUpdated": false
+  },
+  {
+    "image": "drink",
+    "imageUpdated": false,
+    "sound": "drink",
+    "soundUpdated": false,
+    "text": "drink",
+    "textUpdated": false
+  },
+  {
+    "image": "bed",
+    "imageUpdated": false,
+    "sound": "bed",
+    "soundUpdated": false,
+    "text": "bed",
+    "textUpdated": false
+  },
+  {
+    "image": "chair",
+    "imageUpdated": false,
+    "sound": "chair",
+    "soundUpdated": false,
+    "text": "chair",
+    "textUpdated": false
+  },
+  {
+    "image": "table",
+    "imageUpdated": false,
+    "sound": "table",
+    "soundUpdated": false,
+    "text": "table",
+    "textUpdated": false
+  },
+  {
+    "image": "bath",
+    "imageUpdated": false,
+    "sound": "bath",
+    "soundUpdated": false,
+    "text": "bath",
+    "textUpdated": false
+  },
+  {
+    "image": "eyes",
+    "imageUpdated": false,
+    "sound": "eyes",
+    "soundUpdated": false,
+    "text": "eyes",
+    "textUpdated": false
+  },
+  {
+    "image": "mouth",
+    "imageUpdated": false,
+    "sound": "mouth",
+    "soundUpdated": false,
+    "text": "mouth",
+    "textUpdated": false
+  },
+  {
+    "image": "hair",
+    "imageUpdated": false,
+    "sound": "hair",
+    "soundUpdated": false,
+    "text": "hair",
+    "textUpdated": false
+  },
+  {
+    "image": "nose",
+    "imageUpdated": false,
+    "sound": "nose",
+    "soundUpdated": false,
+    "text": "nose",
+    "textUpdated": false
+  },
+  {
+    "image": "spoon",
+    "imageUpdated": false,
+    "sound": "spoon",
+    "soundUpdated": false,
+    "text": "spoon",
+    "textUpdated": false
+  },
+  {
+    "image": "bag",
+    "imageUpdated": false,
+    "sound": "bag",
+    "soundUpdated": false,
+    "text": "bag",
+    "textUpdated": false
+  },
+  {
+    "image": "phone",
+    "imageUpdated": false,
+    "sound": "phone",
+    "soundUpdated": false,
+    "text": "phone",
+    "textUpdated": false
+  },
+  {
+    "image": "bricks",
+    "imageUpdated": false,
+    "sound": "bricks",
+    "soundUpdated": false,
+    "text": "bricks",
+    "textUpdated": false
+  },
+  {
+    "image": "eating",
+    "imageUpdated": false,
+    "sound": "eating",
+    "soundUpdated": false,
+    "text": "eating",
+    "textUpdated": false
+  },
+  {
+    "image": "sleeping",
+    "imageUpdated": false,
+    "sound": "sleeping",
+    "soundUpdated": false,
+    "text": "sleeping",
+    "textUpdated": false
+  },
+  {
+    "image": "drinking",
+    "imageUpdated": false,
+    "sound": "drinking",
+    "soundUpdated": false,
+    "text": "drinking",
+    "textUpdated": false
+  },
+  {
+    "image": "brushing",
+    "imageUpdated": false,
+    "sound": "brushing",
+    "soundUpdated": false,
+    "text": "brushing",
+    "textUpdated": false
+  },
+  {
+    "image": "sitting",
+    "imageUpdated": false,
+    "sound": "sitting",
+    "soundUpdated": false,
+    "text": "sitting",
+    "textUpdated": false
+  },
+  {
+    "image": "washing",
+    "imageUpdated": false,
+    "sound": "washing",
+    "soundUpdated": false,
+    "text": "washing",
+    "textUpdated": false
+  },
+  {
+    "image": "walking",
+    "imageUpdated": false,
+    "sound": "walking",
+    "soundUpdated": false,
+    "text": "walking",
+    "textUpdated": false
+  },
+  {
+    "image": "crying",
+    "imageUpdated": false,
+    "sound": "crying",
+    "soundUpdated": false,
+    "text": "crying",
+    "textUpdated": false
+  },
+  {
+    "image": "television",
+    "imageUpdated": false,
+    "sound": "television",
+    "soundUpdated": false,
+    "text": "television",
+    "textUpdated": false
+  },
+  {
+    "image": "light",
+    "imageUpdated": false,
+    "sound": "light",
+    "soundUpdated": false,
+    "text": "light",
+    "textUpdated": false
+  },
+  {
+    "image": "balloon",
+    "imageUpdated": false,
+    "sound": "balloon",
+    "soundUpdated": false,
+    "text": "balloon",
+    "textUpdated": false
+  },
+  {
+    "image": "box",
+    "imageUpdated": false,
+    "sound": "box",
+    "soundUpdated": false,
+    "text": "box",
+    "textUpdated": false
+  },
+  {
+    "image": "bubbles",
+    "imageUpdated": false,
+    "sound": "bubbles",
+    "soundUpdated": false,
+    "text": "bubbles",
+    "textUpdated": false
+  },
+  {
+    "image": "big",
+    "imageUpdated": false,
+    "sound": "big",
+    "soundUpdated": false,
+    "text": "big",
+    "textUpdated": false
+  },
+  {
+    "image": "splashing",
+    "imageUpdated": false,
+    "sound": "splashing",
+    "soundUpdated": false,
+    "text": "splashing",
+    "textUpdated": false
+  },
+  {
+    "image": "little",
+    "imageUpdated": false,
+    "sound": "little",
+    "soundUpdated": false,
+    "text": "little",
+    "textUpdated": false
+  },
+  {
+    "image": "garden",
+    "imageUpdated": false,
+    "sound": "garden",
+    "soundUpdated": false,
+    "text": "garden",
+    "textUpdated": false
+  },
+  {
+    "image": "star",
+    "imageUpdated": false,
+    "sound": "star",
+    "soundUpdated": false,
+    "text": "star",
+    "textUpdated": false
+  },
+  {
+    "image": "house",
+    "imageUpdated": false,
+    "sound": "house",
+    "soundUpdated": false,
+    "text": "house",
+    "textUpdated": false
+  },
+  {
+    "image": "tree",
+    "imageUpdated": false,
+    "sound": "tree",
+    "soundUpdated": false,
+    "text": "tree",
+    "textUpdated": false
+  },
+  {
+    "image": "jumping",
+    "imageUpdated": false,
+    "sound": "jumping",
+    "soundUpdated": false,
+    "text": "jumping",
+    "textUpdated": false
+  },
+  {
+    "image": "playing",
+    "imageUpdated": false,
+    "sound": "playing",
+    "soundUpdated": false,
+    "text": "playing",
+    "textUpdated": false
+  },
+  {
+    "image": "running",
+    "imageUpdated": false,
+    "sound": "running",
+    "soundUpdated": false,
+    "text": "running",
+    "textUpdated": false
+  },
+  {
+    "image": "kissing",
+    "imageUpdated": false,
+    "sound": "kissing",
+    "soundUpdated": false,
+    "text": "kissing",
+    "textUpdated": false
+  },
+  {
+    "image": "clapping",
+    "imageUpdated": false,
+    "sound": "clapping",
+    "soundUpdated": false,
+    "text": "clapping",
+    "textUpdated": false
+  },
+  {
+    "image": "reading",
+    "imageUpdated": false,
+    "sound": "reading",
+    "soundUpdated": false,
+    "text": "reading",
+    "textUpdated": false
+  },
+  {
+    "image": "cutting",
+    "imageUpdated": false,
+    "sound": "cutting",
+    "soundUpdated": false,
+    "text": "cutting",
+    "textUpdated": false
+  },
+  {
+    "image": "throwing",
+    "imageUpdated": false,
+    "sound": "throwing",
+    "soundUpdated": false,
+    "text": "throwing",
+    "textUpdated": false
+  },
+  {
+    "image": "towel",
+    "imageUpdated": false,
+    "sound": "towel",
+    "soundUpdated": false,
+    "text": "towel",
+    "textUpdated": false
+  },
+  {
+    "image": "soap",
+    "imageUpdated": false,
+    "sound": "soap",
+    "soundUpdated": false,
+    "text": "soap",
+    "textUpdated": false
+  },
+  {
+    "image": "toothbrush",
+    "imageUpdated": false,
+    "sound": "toothbrush",
+    "soundUpdated": false,
+    "text": "toothbrush",
+    "textUpdated": false
+  },
+  {
+    "image": "teeth",
+    "imageUpdated": false,
+    "sound": "teeth",
+    "soundUpdated": false,
+    "text": "teeth",
+    "textUpdated": false
+  },
+  {
+    "image": "bread",
+    "imageUpdated": false,
+    "sound": "bread",
+    "soundUpdated": false,
+    "text": "bread",
+    "textUpdated": false
+  },
+  {
+    "image": "dinner",
+    "imageUpdated": false,
+    "sound": "dinner",
+    "soundUpdated": false,
+    "text": "dinner",
+    "textUpdated": false
+  },
+  {
+    "image": "juice",
+    "imageUpdated": false,
+    "sound": "juice",
+    "soundUpdated": false,
+    "text": "juice",
+    "textUpdated": false
+  },
+  {
+    "image": "yogurt",
+    "imageUpdated": false,
+    "sound": "yogurt",
+    "soundUpdated": false,
+    "text": "yogurt",
+    "textUpdated": false
+  },
+  {
+    "image": "chicken",
+    "imageUpdated": false,
+    "sound": "chicken",
+    "soundUpdated": false,
+    "text": "chicken",
+    "textUpdated": false
+  },
+  {
+    "image": "monkey",
+    "imageUpdated": false,
+    "sound": "monkey",
+    "soundUpdated": false,
+    "text": "monkey",
+    "textUpdated": false
+  },
+  {
+    "image": "rabbit",
+    "imageUpdated": false,
+    "sound": "rabbit",
+    "soundUpdated": false,
+    "text": "rabbit",
+    "textUpdated": false
+  },
+  {
+    "image": "horse",
+    "imageUpdated": false,
+    "sound": "horse",
+    "soundUpdated": false,
+    "text": "horse",
+    "textUpdated": false
+  },
+  {
+    "image": "dress",
+    "imageUpdated": false,
+    "sound": "dress",
+    "soundUpdated": false,
+    "text": "dress",
+    "textUpdated": false
+  },
+  {
+    "image": "jumper",
+    "imageUpdated": false,
+    "sound": "jumper",
+    "soundUpdated": false,
+    "text": "jumper",
+    "textUpdated": false
+  },
+  {
+    "image": "boots",
+    "imageUpdated": false,
+    "sound": "boots",
+    "soundUpdated": false,
+    "text": "boots",
+    "textUpdated": false
+  },
+  {
+    "image": "trousers",
+    "imageUpdated": false,
+    "sound": "trousers",
+    "soundUpdated": false,
+    "text": "trousers",
+    "textUpdated": false
+  },
+  {
+    "image": "foot",
+    "imageUpdated": false,
+    "sound": "foot",
+    "soundUpdated": false,
+    "text": "foot",
+    "textUpdated": false
+  },
+  {
+    "image": "tummy",
+    "imageUpdated": false,
+    "sound": "tummy",
+    "soundUpdated": false,
+    "text": "tummy",
+    "textUpdated": false
+  },
+  {
+    "image": "hand",
+    "imageUpdated": false,
+    "sound": "hand",
+    "soundUpdated": false,
+    "text": "hand",
+    "textUpdated": false
+  },
+  {
+    "image": "ear",
+    "imageUpdated": false,
+    "sound": "ear",
+    "soundUpdated": false,
+    "text": "ear",
+    "textUpdated": false
+  },
+  {
+    "image": "up",
+    "imageUpdated": false,
+    "sound": "up",
+    "soundUpdated": false,
+    "text": "up",
+    "textUpdated": false
+  },
+  {
+    "image": "down",
+    "imageUpdated": false,
+    "sound": "down",
+    "soundUpdated": false,
+    "text": "down",
+    "textUpdated": false
+  },
+  {
+    "image": "swing",
+    "imageUpdated": false,
+    "sound": "swing",
+    "soundUpdated": false,
+    "text": "swing",
+    "textUpdated": false
+  },
+  {
+    "image": "bike",
+    "imageUpdated": false,
+    "sound": "bike",
+    "soundUpdated": false,
+    "text": "bike",
+    "textUpdated": false
+  },
+  {
+    "image": "boat",
+    "imageUpdated": false,
+    "sound": "boat",
+    "soundUpdated": false,
+    "text": "boat",
+    "textUpdated": false
+  },
+  {
+    "image": "bus",
+    "imageUpdated": false,
+    "sound": "bus",
+    "soundUpdated": false,
+    "text": "bus",
+    "textUpdated": false
+  },
+  {
+    "image": "aeroplane",
+    "imageUpdated": false,
+    "sound": "aeroplane",
+    "soundUpdated": false,
+    "text": "aeroplane",
+    "textUpdated": false
+  },
+  {
+    "image": "train",
+    "imageUpdated": false,
+    "sound": "train",
+    "soundUpdated": false,
+    "text": "train",
+    "textUpdated": false
+  },
+  {
+    "image": "wet",
+    "imageUpdated": false,
+    "sound": "wet",
+    "soundUpdated": false,
+    "text": "wet",
+    "textUpdated": false
+  },
+  {
+    "image": "dirty",
+    "imageUpdated": false,
+    "sound": "dirty",
+    "soundUpdated": false,
+    "text": "dirty",
+    "textUpdated": false
+  },
+  {
+    "image": "hot",
+    "imageUpdated": false,
+    "sound": "hot",
+    "soundUpdated": false,
+    "text": "hot",
+    "textUpdated": false
+  },
+  {
+    "image": "cold",
+    "imageUpdated": false,
+    "sound": "cold",
+    "soundUpdated": false,
+    "text": "cold",
+    "textUpdated": false
+  }
+];
