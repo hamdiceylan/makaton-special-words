a3ecb5d50b973120b442a75ca5bf3f12b4f349a8
umut <umutsaydam24@gmail.com>
2025-10-08 19:39:02 +0300

    Refactor: set always light mode

--- hooks/use-color-scheme.ts
+++ hooks/use-color-scheme.ts
@@ -1,1 +1,7 @@
-export { useColorScheme } from 'react-native';
+import { useColorScheme as useRNColorScheme } from 'react-native';
+
+export function useColorScheme(): 'light' {
+  // React Native'in color scheme'ini oku ama her zaman 'light' döndür
+  useRNColorScheme(); // Bu sadece re-render için
+  return 'light';
+}
